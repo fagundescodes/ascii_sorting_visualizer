59c81f88569e39fc55305deebedb0b9fa029465d
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-04-28 12:16:09 -0400

    trying new sorting algo

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,41 @@ fn main() {
     numbers.shuffle(&mut rng);
     let mut my_array: Vec<i32> = numbers[0..10].to_vec();
 
-    bubble_sort(&mut my_array);
+    // bubble_sort(&mut my_array);
+    // selection_sort(&mut my_array);
+    insertion_sort(&mut my_array);
+}
 
-    println!("The array sorted is {:?}", my_array);
+fn insertion_sort(arr: &mut [i32]) {
+    let len = arr.len();
+    for i in 1..len {
+        let key = arr[i];
+        let mut j = i;
+        while j > 0 && arr[j - 1] > key {
+            visualize(arr, Some(j - 1), Some(j));
+            arr[j] = arr[j - 1];
+            j -= 1;
+        }
+        arr[j] = key;
+        visualize(arr, Some(j), None);
+    }
+    visualize(arr, None, None);
+}
+
+fn selection_sort(arr: &mut [i32]) {
+    let len = arr.len();
+    for i in 0..len {
+        let mut min_idx = i;
+        for j in i + 1..len {
+            visualize(arr, Some(min_idx), Some(j));
+            if arr[j] < arr[min_idx] {
+                min_idx = j;
+            }
+        }
+        arr.swap(i, min_idx);
+        visualize(arr, Some(i), Some(min_idx));
+    }
+    visualize(arr, None, None);
 }
 
 fn bubble_sort(arr: &mut [i32]) {
