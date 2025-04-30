9696f43dbd74caefac84ce703fa862dc0dad4fe8
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-04-29 21:44:33 -0400

    what happened here?

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
 use rand::seq::SliceRandom;
+use ratatui::{Terminal, backend::CrosstermBackend};
+use std::io::{self, stdout};
+
 mod sort;
 mod ui;
 
-fn main() {
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+    let mut stdout = stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
     let mut rng = rand::rng();
     let mut numbers: Vec<i32> = (1..=20).collect();
     numbers.shuffle(&mut rng);
@@ -11,4 +25,14 @@ fn main() {
     sort::bubble_sort(&mut my_array);
     // sort::insertion_sort(&mut my_array);
     // sort::selection_sort(&mut my_array);
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
